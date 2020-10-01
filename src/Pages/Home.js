41b29c47520c6c0f4317048b9c3248550f8396c1
unknown <haris.harwinsyah@gmail.com>
2020-10-01 21:10:57 +0800

    landing page

---
+++ src/Pages/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Container, Navbar, Button } from "react-bootstrap";
+
+import logo from "../Assets/Icon/libraryIcon.svg";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar>
+          <Navbar.Brand href="#">
+            <img src={logo} alt="logo" />
+          </Navbar.Brand>
+        </Navbar>
+        <Container fluid>
+          <h1>
+            <i> Your </i> library
+          </h1>
+          <h1> anywhere </h1>
+          <p>
+            Sign - up today and receive unlimited access to all of your reading
+            - share your book.
+          </p>
+          <Button> Sign Up </Button> <Button> Sign In </Button>
+        </Container>
+      </div>
+    );
+  }
+}
